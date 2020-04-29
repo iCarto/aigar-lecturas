516793a7f113917c5bee5c6187d2ed363bbb6d11
Pablo Sanxiao <psanxiao@icarto.es>
2020-04-29 13:20:14 +0200

    Updated alert message when users file is imported

--- src/htmlTemplates.js
+++ src/htmlTemplates.js
@@ -132,7 +132,7 @@ export const fileWrittenAlertTemplate = `
 
 export const dataImportedAlertTemplate = `
 <div class="alert alert-warning alert-dismissible fade show" role="alert">
-<strong>Datos importados correctamente desde el fichero</strong>
+<strong>Datos importados correctamente desde el fichero. Se empieza un nuevo ciclo de lecturas.</strong>
 <button type="button" class="close" data-dismiss="alert" aria-label="Close">
   <span aria-hidden="true">&times;</span>
 </button>
