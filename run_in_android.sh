a8908b1319622c89a19b8cfb62220d792b0e46fc
Francisco Puga <fpuga@icarto.es>
2024-09-02 19:03:07 +0200

    Fixes run_in_android.sh

--- run_in_android.sh
+++ run_in_android.sh
@@ -7,8 +7,8 @@ rm lecturas_hechas.json
 adb shell rm "${LECTURAS_HECHAS}"
 adb shell rm "${LECTURAS}"
 
-adb push samples/lecturas.json "${LECTURAS}"
 
 cordova build android
 
 adb -s e5694295 install ./platforms/android/app/build/outputs/apk/debug/app-debug.apk
+adb push samples/lecturas.json "${LECTURAS}"
